fd345f158f32ebe9c59f96480710dcbf9d32073f
indrabsudirman <indrabsudirman@gmail.com>
2022-02-14 17:15:12 +0700

    Add func Auth token

---
+++ handler/auth-handler.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"go-fiber/database"
+	"go-fiber/model/entity"
+	"go-fiber/model/request"
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+func LoginHandler(ctx *fiber.Ctx) error {
+
+	loginRequest := new(request.LoginRequest)
+	if err := ctx.BodyParser(loginRequest); err != nil {
+		return err
+	}
+
+	log.Println(loginRequest)
+
+	//Validation Request
+	validate := validator.New()
+	errValidate := validate.Struct(loginRequest)
+	if errValidate != nil {
+		return ctx.Status(400).JSON(fiber.Map{
+			"message": "failed",
+			"error":   errValidate.Error(),
+		})
+	}
+
+	var user entity.User
+
+	err := database.DB.First(&user, "email = ?", loginRequest.Email).Error
+	if err != nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "wrong credential",
+		})
+	}
+
+	return ctx.JSON(fiber.Map{
+		"token": "secret",
+	})
+}
