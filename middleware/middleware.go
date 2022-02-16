27bc8e3708bb258a23a7e176c587b1781760e2af
Indra Sudirman <indrabsudirman@gmail.com>
2022-02-16 11:00:08 +0700

    Add VerifyToken

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,14 +1,33 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"go-fiber/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Auth(ctx *fiber.Ctx) error {
 	token := ctx.Get("key-access")
-	if token != "Indra19" {
+
+	if token == "" {
 		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"message": "unauthenticated",
 		})
 	}
+
+	_, err := utils.VerifyToken(token)
+
+	if err != nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	// if token != "Indra19" {
+	// 	return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+	// 		"message": "unauthenticated",
+	// 	})
+	// }
 
 	return ctx.Next()
 }
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,10 @@
 package utils
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v4"
+)
 
 var SecretKey = "SECRET_TOKEN"
 
@@ -13,4 +17,22 @@ func GenerateToken(claims *jwt.MapClaims) (string, error) {
 	}
 
 	return webToken, nil
+}
+
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+	t, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, isValid := t.Method.(*jwt.SigningMethodHMAC); !isValid {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte(SecretKey), nil
+
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+
 }
